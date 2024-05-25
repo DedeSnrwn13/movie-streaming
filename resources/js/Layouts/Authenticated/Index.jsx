66f8dc04b4e6d6aef3f505aa9d6c6a7290adb208
DedeSnrwn13 <sunarwandede13@gmail.com>
2024-05-25 08:35:19 +0700

    feat: admin init create movie

--- resources/js/Layouts/Authenticated/Index.jsx
+++ resources/js/Layouts/Authenticated/Index.jsx
@@ -1,7 +1,7 @@
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 
-export default function Autenticated({ auth, children }) {
+export default function Authenticated({ auth, children }) {
     return (
         <>
             <div className="mx-auto max-w-screen hidden lg:block">
--- resources/js/Pages/Admin/Movie/Index.jsx
+++ resources/js/Pages/Admin/Movie/Index.jsx
@@ -1,3 +1,16 @@
-export default function Index() {
-    return <div>Admin</div>;
+import React from "react";
+import PrimaryButton from "@/Components/PrimaryButton";
+import Authenticated from "@/Layouts/Authenticated/Index";
+import { Link } from "@inertiajs/react";
+
+export default function Index({ auth }) {
+    return (
+        <Authenticated auth={auth}>
+            <Link href={route("admin.dashboard.movie.create")}>
+                <PrimaryButton type={"button"} className={"w-40 mb-8"}>
+                    Insert New Movie
+                </PrimaryButton>
+            </Link>
+        </Authenticated>
+    );
 }
