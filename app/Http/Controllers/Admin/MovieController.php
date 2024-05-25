7c697ea6c5fb6c1e17d2ca61b540a4c8008b2f6b
DedeSnrwn13 <sunarwandede13@gmail.com>
2024-05-25 09:26:51 +0700

    feat: admin movie store validation

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Movie\StoreRequest;
 use App\Models\Movie;
 use Illuminate\Http\Request;
 
@@ -27,7 +28,7 @@ class MovieController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request)
     {
         return $request->all();
     }
