22cb6878463a133ed22fccce54021e57317b45f7
DedeSnrwn13 <sunarwandede13@gmail.com>
2024-05-25 09:43:16 +0700

    feat: admin flash message

--- resources/js/Pages/Admin/Movie/Index.jsx
+++ resources/js/Pages/Admin/Movie/Index.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import PrimaryButton from "@/Components/PrimaryButton";
 import Authenticated from "@/Layouts/Authenticated/Index";
 import { Link } from "@inertiajs/react";
+import FlashMessage from "@/Components/FlashMessage";
 
-export default function Index({ auth }) {
+export default function Index({ auth, flashMessage }) {
     return (
         <Authenticated auth={auth}>
             <Link href={route("admin.dashboard.movie.create")}>
@@ -11,6 +12,10 @@ export default function Index({ auth }) {
                     Insert New Movie
                 </PrimaryButton>
             </Link>
+
+            {flashMessage?.message && (
+                <FlashMessage message={flashMessage.message} />
+            )}
         </Authenticated>
     );
 }
